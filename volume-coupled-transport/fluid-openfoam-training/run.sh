6f673c7827d11412f415e83789d255dae3fba40a
Daniels <d.vidulejs@tum.de>
2025-06-05 17:29:15 +0200

    delete

--- volume-coupled-transport/fluid-openfoam-training/run.sh
+++ volume-coupled-transport/fluid-openfoam-training/run.sh
@@ -1,1 +1,13 @@
-../fluid-openfoam/run.sh
+#!/bin/bash
+set -e -u
+
+. ../../tools/log.sh
+exec > >(tee --append "$LOGFILE") 2>&1
+
+blockMesh
+topoSet
+
+../../tools/run-openfoam.sh "$@"
+. ../../tools/openfoam-remove-empty-dirs.sh && openfoam_remove_empty_dirs
+
+close_log
